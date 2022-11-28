37d7bd9392b86bdfce340db78e4bcc4cae24852b
ayesha <ayesha@gmail.com>
2022-11-28 16:38:16 +0000

    c

---
+++ buildscripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install default-jdk
+sudo apt install maven -y
+sudo rm -rf hello-world-war
+sudo git clone https://github.com/syedwaliuddin/hello-world-war.git
+mvn package
